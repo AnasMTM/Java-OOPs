68d7260d250cdc4d05fb508f2c9b051a03199c0a
Anas M.T.M <mtmanas125@gmail.com>
2024-11-27 21:27:50 +0530

    Create FictionBook.java

---
+++ Lab worksheet 5/Library/book/FictionBook.java
@@ -0,0 +1,12 @@
+public class FictionBook {
+    String title;
+    String author;
+    String genre;
+
+
+    public void displayInfo(){
+        System.out.println("Title of the book\t : " + title);
+        System.out.println("Author name \t\t : " + author);
+        System.out.println("Genre \t\t\t : " + genre);
+    }
+}

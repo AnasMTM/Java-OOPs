8acda14a150a3c5337ab6dec4edd4294cc877117
Anas M.T.M <mtmanas125@gmail.com>
2024-11-27 21:32:53 +0530

    Create Constants.java

---
+++ Lab worksheet 5/Math/Constants.java
@@ -0,0 +1,8 @@
+public class Constants {
+    final double PI =  3.14159;
+    final int SPEED_OF_LIGHT = 299792458;
+
+    public Constants(double pera) {
+        final double GRAVITY = pera;
+    }
+}
